44e45828294ebe02e1d7123e86885452b86e6bb2
SDKoncepts <akin.anenih@sdkoncept.com>
2026-01-01 23:07:46 +0100

    Fix auth configuration with fallback secret

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -38,5 +38,5 @@ export const authOptions: NextAuthOptions = {
     session: {
         strategy: "jwt",
     },
-    secret: process.env.NEXTAUTH_SECRET || "jkhadsf897234khjasdf789234879", // Fallback for dev
+    secret: process.env.NEXTAUTH_SECRET || "dc1e864f6e79e0542bb4a402351052b4eff00c5c973929bc88e5eada2a1e0d58",
 };
--- src/middleware.ts
+++ src/middleware.ts
@@ -4,6 +4,7 @@ export default withAuth({
     pages: {
         signIn: "/login",
     },
+    secret: process.env.NEXTAUTH_SECRET || "dc1e864f6e79e0542bb4a402351052b4eff00c5c973929bc88e5eada2a1e0d58",
 });
 
 export const config = {
